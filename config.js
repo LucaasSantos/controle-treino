7c2283b2ef4546df323b6abfcbb038b435c72354
LucaasSantos <148167204+LucaasSantos@users.noreply.github.com>
2026-08-24 12:42:40 -0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Cole aqui a URL da implantação do Google Apps Script.
-// Exemplo: https://script.google.com/macros/s/SEU_ID/exec
-const API_URL = "https://script.google.com/macros/s/AKfycbzyCZApkumifcqQOFGWx7vXMXJ-9g2ROjMgMi_qobuHaO-xtTOFNZZlrWZ9Ev7-1fsp/exec";
